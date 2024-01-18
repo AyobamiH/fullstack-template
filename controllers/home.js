625071cc6b566bad78b4124489bc961cd03dd400
AyobamiH <heryourbarme@live.com>
2024-01-18 14:17:35 +0000

    Add new routes for feedback, thank you, about, and disclaimer pages in home controller.

--- controllers/home.js
+++ controllers/home.js
@@ -2,4 +2,20 @@ module.exports = {
   getIndex: (req, res) => {
     res.render("index.ejs");
   },
+
+  getFeedback: (req, res) => {
+    res.render("feedback.ejs");
+  },
+
+  getThankyou: (req, res) => {
+    res.render("thankyou.ejs")
+  },
+  
+  getAbout: (req, res) => {
+    res.render("about.ejs")
+  },
+
+  getDisclaimer: (req, res)=>{
+    res.render("disclaimer.ejs")
+  }
 };
