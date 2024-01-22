3e21c346d45f84b345b7373c525c9b6524f020ed
AyobamiH <heryourbarme@live.com>
2024-01-22 14:54:40 +0000

    Refactor main.js for improved structure and added functionality
    Organized and simplified main routes
    Added new routes for feedback, about, thank you, disclaimer, registered user search, fetching NASA images by date range, and user feed
    Moved profile edit route to a dedicated endpoint
    Enhanced middleware import for better code readability

--- routes/main.js
+++ routes/main.js
@@ -3,17 +3,25 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
-const { ensureAuth } = require("../middleware/auth");
 
-//Main Routes 
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+// const fetchAstronomyImagescontroller = require("../controllers/fetchAstronomyImagesController") // Import the function
+// console.log(fetchAstronomyImagescontroller.fetchAstronomyImages())
+//Main Routes - simplified for now
 router.get("/", homeController.getIndex);
-router.get("/profile", ensureAuth, postsController.getProfile);
-
-//Routes for user login/signup
+router.get("/feedback", homeController.getFeedback);
+router.get("/about", homeController.getAbout);
+router.get("/thankyou", homeController.getThankyou)
+router.get("/disclaimer", homeController.getDisclaimer)
+router.get("/registered-user-search", ensureAuth, postsController.getRegisteredUserSearch);
+router.get("/fetchNasaImagesByDateRange", ensureAuth, postsController.fetchNasaImagesByDateRange)
+router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+router.get("/profile/editprofile", ensureAuth, postsController.editProfile);
 
 module.exports = router;
